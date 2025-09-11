a5fe7b0a3d1f7069585a2d826b8d26772416d8d3
joaopedroBP <jpbazolip@gmail.com>
2025-09-11 17:24:34 -0300

    Criei o yaml para iniciar os bancos com o docker e testei incialmente o postgre com java

---
+++ Postgre_Connection/Main.java
@@ -0,0 +1,26 @@
+/*
+ *Compile com: javac -cp "postgresql-42.7.3.jar" Main.java
+ *Rode com java -cp ".:postgresql-42.7.3.jar" Main
+ * SOMENTE LINUX
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main{
+  public static void main(String[] args){
+    String url = "jdbc:postgresql://localhost:5432/postgres";
+    String usuario = "postgres";
+    String senha = "postgres";
+
+    try (Connection con = DriverManager.getConnection(url,usuario,senha)){
+      if(con != null){
+        System.out.println("Conectado ao PostgreSQL");
+      }
+    }catch (Exception e){
+      System.out.println("Erro ao conectar-se ao PostgreSQL");
+      e.printStackTrace();
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Banco-de-Dados-Polyglot-Persistence-
-Proejto com multiplos bancos de dados para a faculdade.
+# Banco-de-Dados-Polyglot-Persistence.
+
+Augusto Pereira Teixeira - 24.123.008-5
+João Pedro Bazoli Palma - 24.123.041-6
+
+##Instalação e uso
+Tenha Docker Instalado
+Tenha Java instalado
+(De preferência ambos em versões recentes)
+
+inicie os bancos com:
+
+'''bash
+docker compose up -d
+'''
+
+Teste a conexão do PostgreSQL no java executando os seguintes comandos na pasta 'Postgre_Connection'
+
+'''bash
+javac -cp "postgresql-42.7.3.jar" Main.java
+java -cp ".:postgresql-42.7.3.jar" Main
+'''
