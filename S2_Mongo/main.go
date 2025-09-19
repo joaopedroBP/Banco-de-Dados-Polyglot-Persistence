fb64c0a122bd8f559330b4d52807b2aeab69d304
joaopedroBP <jpbazolip@gmail.com>
2025-09-19 12:42:08 -0300

    Consegui fazer com que a conexão com o mongo fosse efetuada, e iniciei o S2_Scylla com cargo new

--- S2_Mongo/main.go
+++ S2_Mongo/main.go
@@ -4,32 +4,21 @@ import(
 	"context"
 	"fmt"
 	"log"
-	"os"
-	"encoding/json"
 
-	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func main(){
-	clientOptions := options.Client().ApplyURI("mongoDB://localhost:27017")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.second)
-
-	defer cancel()
-	client,err := mongo.Connect(ctx,clientOptions)
-	if err != nil {
-		log.fatal(err)
-	}
-
-	err = client.Ping(ctx, readpref.Primary())
+	client, err := mongo.Connect(options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil{
 		log.Fatal(err)
 	}
-
-	fmt.Println("Conectado com sucesso!")
-
 	
+	err = client.Ping(context.TODO(),nil)
+	if err != nil {
+		log.Fatal("falha ao conctar")
+	}
+	fmt.Println("conectado")
 }
 
