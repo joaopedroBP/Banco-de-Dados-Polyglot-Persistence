339a4c11694b8e9e100ed7fc904b92fa626947db
joaopedroBP <jpbazolip@gmail.com>
2025-11-11 15:20:01 -0300

    fiz: .sh pra usar o postgre do S1, criação das tabelas e função de criar user

---
+++ S1/s2postgre.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ..
+cd S2_Postgre
+javac -cp "postgresql-42.7.3.jar" Main.java
+java -cp ".:postgresql-42.7.3.jar" Main $1 $2 $3 $4 $5 $6 $7
--- S2_Postgre/Main.java
+++ S2_Postgre/Main.java
@@ -6,16 +6,91 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;  
+import java.sql.SQLException;     
+
 
 public class Main{
+    public static void createUser(Connection con, String userId, String email, String senha) {
+        String sql = "INSERT INTO users (user_id, email, senha) VALUES (?, ?, ?);";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1, userId);
+            pstmt.setString(2, email);
+            pstmt.setString(3, senha);
+            pstmt.executeUpdate();
+            System.out.println("Usuário '" + userId + "' criado com sucesso!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao criar usuário '" + userId + "':");
+            e.printStackTrace();
+        }
+    }
+  
   public static void main(String[] args){
     String url = "jdbc:postgresql://localhost:5432/postgres";
     String usuario = "postgres";
     String senha = "postgres";
 
+    String function_called = "" + args[0] + args[1];
+    System.out.println("função chamada = " + function_called);
+
     try (Connection con = DriverManager.getConnection(url,usuario,senha)){
       if(con != null){
         System.out.println("Conectado ao PostgreSQL");
+            try (Statement stmt = con.createStatement()) {
+                    String createUsers = """
+                        CREATE TABLE IF NOT EXISTS users (
+                            user_id VARCHAR(100) PRIMARY KEY,  -- nome do usuário
+                            email VARCHAR(100) NOT NULL UNIQUE,
+                            senha VARCHAR(100) NOT NULL,
+                            data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                        );
+                        """;
+
+                    String createArtist = """
+                        CREATE TABLE IF NOT EXISTS artists (
+                            artist_id VARCHAR(100) PRIMARY KEY,  -- nome do artista
+                            user_id VARCHAR(100) REFERENCES users(user_id) ON DELETE CASCADE,
+                            descricao TEXTj  
+                        );
+                        """;
+
+                    String createGenre = """
+                        CREATE TABLE IF NOT EXISTS genres (
+                            genre_id SERIAL PRIMARY KEY,
+                            nome VARCHAR(100) NOT NULL UNIQUE,
+                            descricao TEXT 
+                        );
+                        """;
+
+                    String createAlbum = """
+                        CREATE TABLE IF NOT EXISTS albums (
+                            album_id SERIAL PRIMARY KEY,
+                            artist_id VARCHAR(100) REFERENCES artist(artist_id) ON DELETE CASCADE,
+                            genre_id INT REFERENCES genre(genre_id),
+                            titulo VARCHAR(100) NOT NULL,
+                            data_lancamento DATE,
+                            capa_url TEXT
+                        );
+                        """;
+
+                    String createTrack = """
+                        CREATE TABLE IF NOT EXISTS tracks (
+                            track_id SERIAL PRIMARY KEY,
+                            album_id INT REFERENCES album(album_id) ON DELETE CASCADE,
+                            titulo VARCHAR(100) NOT NULL,
+                            duracao_segundos INT, 
+                            numero_faixa INT 
+                        );
+                        """;
+
+                    stmt.execute(createUsers);
+                    stmt.execute(createArtist);
+                    stmt.execute(createGenre);
+                    stmt.execute(createAlbum);
+                    stmt.execute(createTrack);
+
+          }       
       }
     }catch (Exception e){
       System.out.println("Erro ao conectar-se ao PostgreSQL");
@@ -23,3 +98,4 @@ public class Main{
     }
   }
 }
+
